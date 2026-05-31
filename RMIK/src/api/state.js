eacfa1a93d4ec53893ee88299242a0479d4564b2
simeka99 <engineering.yn20@gmail.com>
2026-06-01 01:40:47 +0700

    remove all project files

--- RMIK/src/api/state.js
+++
@@ -1,4 +0,0 @@
-// Shared state antara worker dan handler
-export const worker_state = {
-    should_run: true,
-};
--- describe.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'd:/GYA TECH INDONESIA/HOSTING SHARED/rmik.gyatechindonesia.com/config/db.php';
-$db = db();
-$stmt = $db->query('DESCRIBE users');
-$columns = $stmt->fetchAll(PDO::FETCH_ASSOC);
-print_r($columns);
